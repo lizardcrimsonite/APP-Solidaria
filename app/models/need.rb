c3b0a3d9508761776e861dd02b59327fbf502968
Claudio <fukudesignandco@gmail.com>
2024-10-25 17:30:37 -0300

    se actualiza modelo y se establece la relacion para conectar ayudas a necesidades especifícas

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,5 +1,6 @@
 class Need < ApplicationRecord
   belongs_to :user
+  has_many :helps, dependent: :destroy
   enum status: { open: 0, in_progress: 1, completed: 2 }
 
   # Validaciones y relaciones aquí
---
+++ db/migrate/20241025202712_add_need_to_helps.rb
@@ -0,0 +1,5 @@
+class AddNeedToHelps < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :helps, :need, null: false, foreign_key: true
+  end
+end
